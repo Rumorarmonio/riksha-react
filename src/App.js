41977052e29bcd4beea12d4c4ae1a4bbf7e26f04
Egor Skleynov <yxllwllxy@gmail.com>
2023-09-07 19:29:40 +0800

    Switched to HashRouter, add redirect from 404 page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div className="App">
             <Header/>
             <Routes>
-                <Route path="/riksha-react" element={<Catalog/>}/>
+                <Route path="/" element={<Catalog/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
             <Footer/>
