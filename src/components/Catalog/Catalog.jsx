ca6fc6acedd68e8107d17b506e999f0464364504
Egor Skleynov <yxllwllxy@gmail.com>
2023-09-24 14:28:12 +0800

    Add qs library, getting params from query string

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import axios from 'axios'
+import qs from 'qs'
+import {useNavigate} from 'react-router-dom'
 import {useDispatch, useSelector} from 'react-redux'
 
 import InfoBlock from './InfoBlock/InfoBlock'
 import Sort from './Sort/Sort'
 import FoodList from './FoodList/FoodList'
-import {setCategoryId, setCurrentPage} from '../../redux/slices/filterSlice'
+import {setCategoryId, setCurrentPage, setFilters} from '../../redux/slices/filterSlice'
+import {sortTypes} from '../../assets/data/arrays'
 
 export const FilterContext = React.createContext()
 
 export default function Catalog() {
-    const {categoryId, sortType, currentPage} = useSelector(state => state.filter)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const isSearch = React.useRef(false)
+    const isMounted = React.useRef(false)
+
+    const {categoryId, sortType, currentPage} = useSelector(state => state.filter)
 
     const onChangeCategory = (id) => {
         dispatch(setCategoryId(id))
@@ -28,7 +36,7 @@ export default function Catalog() {
     // TODO: count pages in code and remove hardcoded number
     const [searchValue, setSearchValue] = React.useState('')
 
-    React.useEffect(() => {
+    const fetchItems = () => {
         setIsLoading(true)
 
         const sortBy = sortType.sortProperty.replace('-', '')
@@ -43,8 +51,45 @@ export default function Catalog() {
                 setIsLoading(false)
             }
         )
+    }
+
+    React.useEffect(() => {
+        if (window.location.search) {
+            const params = qs.parse(window.location.search.substring(1))
+
+            const sortType = sortTypes.find(obj => obj.sortProperty === params.sortProperty)
+
+            dispatch(
+                setFilters({
+                    ...params,
+                    sortType
+                })
+            )
+            isSearch.current = true
+        }
+    }, [])
+
+    React.useEffect(() => {
+        if (isMounted.current) {
+            const queryString = qs.stringify({
+                sortProperty: sortType.sortProperty,
+                categoryId,
+                currentPage,
+            })
+            navigate(`?${queryString}`)
+        }
+        isMounted.current = true
+    }, [categoryId, sortType.sortProperty, searchValue, currentPage])
+
+    React.useEffect(() => {
         // window.scrollTo(0, 0)
-    }, [categoryId, sortType, searchValue, currentPage])
+
+        if (!isSearch.current) {
+            fetchItems()
+        }
+
+        isSearch.current = false
+    }, [categoryId, sortType.sortProperty, searchValue, currentPage])
 
     return (
         <>
